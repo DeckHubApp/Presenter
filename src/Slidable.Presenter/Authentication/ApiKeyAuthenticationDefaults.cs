21d6c7d789fd333c90782cdfd9a81ab0b52080c6
Mark Rendle <mark@rendlelabs.com>
2018-03-10 22:36:18 +0000

    Initial commit

---
+++ src/Slidable.Presenter/Authentication/ApiKeyAuthenticationDefaults.cs
@@ -0,0 +1,7 @@
+﻿namespace Slidable.Presenter.Authentication
+{
+    public static class ApiKeyAuthenticationDefaults
+    {
+        public const string AuthenticationScheme = "ApiKey";
+    }
+}
---
+++ src/Slidable.Presenter/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Slidable.Presenter.Authentication;
+
+namespace Slidable.Presenter
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
+        [UsedImplicitly]
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = ApiKeyAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddApiKey(o => o.ApiKeyHashPhrase = Configuration["Security:ApiKeyHashPhrase"])
+                .AddCookie();
+        }
+
+        [UsedImplicitly]
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
